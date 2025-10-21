29b2dc09eff0523fab80bd5416448146d6a45fbc
Clovett <blanket@mailfence.com>
2025-10-21 15:33:56 +0300

    feat: rm divide price by quantity

--- dist/EasyShipEbay-CopyPaste.user.js
+++ dist/EasyShipEbay-CopyPaste.user.js
@@ -193,7 +193,9 @@
     );
     const orderItems = document.querySelectorAll(".item-card");
     const readyToCopyArr = [];
+    const readyToSheetsArr = [];
     let totalQuantity = 0;
+    let positionsCount = 0;
     orderItems.forEach((item) => {
       const title = item.querySelector(".item-title .eui-text-span span")?.textContent || "";
       const aspectValues = item.querySelectorAll(".item-aspect-value");
@@ -202,20 +204,31 @@
       const price = item.querySelector(".item-price .eui-text-span span")?.textContent || "";
       const link = item.querySelector(".item-page-content-link")?.href || "";
       const brand = "";
-      const handledPrice = quantityElement ? parseFloat(formatPrice(price)) / Number(quantity) : formatPrice(price);
+      const priceForOneItem = quantityElement ? parseFloat(formatPrice(price)) / Number(quantity) : formatPrice(price);
       totalQuantity += Number(quantity);
+      positionsCount++;
       readyToCopyArr.push({
         title,
-        price: String(handledPrice),
+        price: String(priceForOneItem),
+        quantity,
+        link,
+        brand
+      });
+      readyToSheetsArr.push({
+        title,
+        priceForAllItems: formatPrice(price),
         quantity,
         link,
         brand
       });
     });
+    log.info("\u{1F4E6} \u0421\u043E\u0431\u0440\u0430\u043D\u043D\u044B\u0435 \u0434\u0430\u043D\u043D\u044B\u0435 \u0434\u043B\u044F \u0442\u0430\u0431\u043B\u0438\u0446 (readyToSheetsArr):");
+    log.info(readyToSheetsArr);
     let shippingPerItem = 0;
     try {
       log.info("\u{1F50D} \u041D\u0430\u0447\u0438\u043D\u0430\u0435\u043C \u0438\u0441\u043A\u0430\u0442\u044C \u0434\u043E\u0441\u0442\u0430\u0432\u043A\u0443...");
-      log.info(`\u{1F4E6} \u041E\u0431\u0449\u0435\u0435 \u043A\u043E\u043B\u0438\u0447\u0435\u0441\u0442\u0432\u043E \u043F\u043E\u0437\u0438\u0446\u0438\u0439: ${totalQuantity}`);
+      log.info(`\u{1F4E6} \u041E\u0431\u0449\u0435\u0435 \u043A\u043E\u043B\u0438\u0447\u0435\u0441\u0442\u0432\u043E \u0442\u043E\u0432\u0430\u0440\u043E\u0432: ${totalQuantity}`);
+      log.info(`\u{1F4E6} \u041A\u043E\u043B\u0438\u0447\u0435\u0441\u0442\u0432\u043E \u043F\u043E\u0437\u0438\u0446\u0438\u0439 (\u0441\u0442\u0440\u043E\u043A): ${positionsCount}`);
       const paymentLineItems = document.querySelector(".payment-line-items");
       log.info(`\u{1F4CB} \u041D\u0430\u0439\u0434\u0435\u043D payment-line-items: ${!!paymentLineItems}`);
       if (paymentLineItems) {
@@ -238,7 +251,7 @@
               log.info(`\u{1F522} totalShipping (\u0447\u0438\u0441\u043B\u043E): ${totalShipping}`);
               if (!isNaN(totalShipping) && totalShipping > 0 && totalQuantity > 0) {
                 shippingPerItem = totalShipping / totalQuantity;
-                log.info(`\u2728 \u0414\u043E\u0441\u0442\u0430\u0432\u043A\u0430 \u043D\u0430 \u043E\u0434\u043D\u0443 \u043F\u043E\u0437\u0438\u0446\u0438\u044E: ${shippingPerItem}`);
+                log.info(`\u2728 \u0414\u043E\u0441\u0442\u0430\u0432\u043A\u0430 \u043D\u0430 \u043E\u0434\u0438\u043D \u0442\u043E\u0432\u0430\u0440: ${shippingPerItem}`);
               } else {
                 log.warn({
                   message: "\u274C \u041D\u0435 \u043F\u0440\u043E\u0448\u043B\u0430 \u043F\u0440\u043E\u0432\u0435\u0440\u043A\u0430",
@@ -254,16 +267,18 @@
       } else {
         log.warn("\u274C payment-line-items \u043D\u0435 \u043D\u0430\u0439\u0434\u0435\u043D");
       }
-      log.info(`\u{1F4CA} \u0418\u0442\u043E\u0433\u043E\u0432\u0430\u044F \u0434\u043E\u0441\u0442\u0430\u0432\u043A\u0430 \u043D\u0430 \u043F\u043E\u0437\u0438\u0446\u0438\u044E: ${shippingPerItem}`);
+      log.info(`\u{1F4CA} \u0418\u0442\u043E\u0433\u043E\u0432\u0430\u044F \u0434\u043E\u0441\u0442\u0430\u0432\u043A\u0430 \u043D\u0430 \u043E\u0434\u0438\u043D \u0442\u043E\u0432\u0430\u0440: ${shippingPerItem}`);
     } catch (e) {
       log.error(`\u274C \u041E\u0448\u0438\u0431\u043A\u0430 \u043F\u0440\u0438 \u043F\u043E\u043B\u0443\u0447\u0435\u043D\u0438\u0438 \u0441\u0442\u043E\u0438\u043C\u043E\u0441\u0442\u0438 \u0434\u043E\u0441\u0442\u0430\u0432\u043A\u0438: ${e}`);
     }
-    const sheetsDataArr = readyToCopyArr.map((item) => ({
-      title: item.title,
-      quantity: item.quantity,
-      price: item.price,
-      shipping: shippingPerItem > 0 ? shippingPerItem.toFixed(2) : ""
-    }));
+    const sheetsDataArr = readyToSheetsArr.map((item) => {
+      return {
+        title: item.title,
+        quantity: item.quantity,
+        price: item.priceForAllItems,
+        shipping: shippingPerItem ? shippingPerItem.toFixed(2) : ""
+      };
+    });
     log.info("\u{1F4CB} \u041C\u0430\u0441\u0441\u0438\u0432 \u0434\u043B\u044F \u0442\u0430\u0431\u043B\u0438\u0446 (sheetsDataArr):");
     log.info(sheetsDataArr);
     try {
